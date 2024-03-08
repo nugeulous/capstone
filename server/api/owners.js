2ed573068ff49adc9cd6df3ff8cca47b8f592728
khirym <khmaynard@gmail.com>
2024-03-07 17:43:10 -0800

    new branch

--- server/api/owners.js
+++ server/api/owners.js
@@ -3,8 +3,8 @@ const ownersRouter = express.Router();
 
 const { 
   createOwner,
-  getAllOwners
-  // getOwnerById
+  getAllOwners,
+  getOwnerById
 } = require('../db');
 
 ownersRouter.get('/', async (req, res, next) => {
@@ -16,6 +16,34 @@ ownersRouter.get('/', async (req, res, next) => {
       });
     } catch ({ name, message }) {
       next({ name, message });
+    }
+  });
+
+ownersRouter.post('/', async (req, res, next) => {
+    try {
+      const owners = await createOwner();
+  
+      res.send({
+        owners
+      });
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
+
+  ownersRouter.get('/:id', async (req, res, next) => {
+    try {
+      const ownerId = req.params.id;
+      const owner = await getOwnerById(ownerId);
+
+      if (!owner) {
+        res.status(404).send({ message: 'Owner not found' });
+        return;
+      }
+
+      res.send({ owner });
+    } catch (error) {
+      next(error);
     }
   });
 
--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const path = require('path')
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require("dotenv").config();
+const client = require("./db");
 
 const PORT = 8080;
 
+//routes
+const ownersRouter = require('./api');
+server.use('/api', ownersRouter);
+
+// middleware
 app.use(cors());
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "..", "client/dist")));
