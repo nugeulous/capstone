f0e41bd8b82e847c8bd220c9f1acd245544eabdd
khirym <khmaynard@gmail.com>
2024-03-07 18:35:22 -0800

    route working

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,13 @@
 const { Client } = require("pg"); // imports the pg module
 
 const client = new Client({
-  user: "khirymaynard",
-  password: "",
-  host: "localhost",
-  port: 5432,
-  database: "alltails"
-  });
+  connectionString:
+      process.env.DATABASE_URL || "postgresql://localhost:5432/alltails",
+  ssl:
+      process.env.NODE_ENV === "production"
+          ? { rejectUnauthorized: false }
+          : undefined,
+});
 
   async function createOwner({ email, password, fname, lname, location, phone, image, gender }) {
     try {
