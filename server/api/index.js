d5116e85a2781feb1bf27654c5e5d837a186c15a
monica bobadilla <bobadillamonica5@gmail.com>
2024-03-11 20:22:51 -0400

    updated api (index.js/petsitters.js), db (index.js, seed.js)

--- server/api/index.js
+++ server/api/index.js
@@ -3,61 +3,61 @@ const express = require('express');
 const apiRouter = express.Router();
 
 
-const { getUserById } = require('../db');
+// const { getUserById } = require('../db');
 
-// set `req.user` if possible
-apiRouter.use(async (req, res, next) => {
-  const prefix = 'Bearer ';
-  const auth = req.header('Authorization');
+// // set `req.user` if possible
+// apiRouter.use(async (req, res, next) => {
+//   const prefix = 'Bearer ';
+//   const auth = req.header('Authorization');
 
-  if (!auth) {
-    // nothing to see here
-    next();
-  } else if (auth.startsWith(prefix)) {
-    const token = auth.slice(prefix.length);
+//   if (!auth) {
+//     // nothing to see here
+//     next();
+//   } else if (auth.startsWith(prefix)) {
+//     const token = auth.slice(prefix.length);
 
-    try {
-      const { id } = jwt.verify(token, JWT_SECRET);
+//     try {
+//       const { id } = jwt.verify(token, JWT_SECRET);
 
-      if (id) {
-        req.user = await getUserById(id);
-        next();
-      } else {
-        next({
-          name: 'AuthorizationHeaderError',
-          message: 'Authorization token malformed',
-        });
-      }
-    } catch ({ name, message }) {
-      next({ name, message });
-    }
-  } else {
-    next({
-      name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with ${prefix}`,
-    });
-  }
-});
+//       if (id) {
+//         req.user = await getUserById(id);
+//         next();
+//       } else {
+//         next({
+//           name: 'AuthorizationHeaderError',
+//           message: 'Authorization token malformed',
+//         });
+//       }
+//     } catch ({ name, message }) {
+//       next({ name, message });
+//     }
+//   } else {
+//     next({
+//       name: 'AuthorizationHeaderError',
+//       message: `Authorization token must start with ${prefix}`,
+//     });
+//   }
+// });
 
-apiRouter.use((req, res, next) => {
-  if (req.user) {
-    console.log('User is set:', req.user);
-  }
+// apiRouter.use((req, res, next) => {
+//   if (req.user) {
+//     console.log('User is set:', req.user);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 // ROUTER: /api/owners
 const ownersRouter = require('./owners');
 apiRouter.use('/owners', ownersRouter);
 
-// // ROUTER: /api/posts
-// const postsRouter = require('./posts');
-// apiRouter.use('/posts', postsRouter);
+// ROUTER: /api/pets
+const petsRouter = require('./pets');
+apiRouter.use('/pets', petsRouter);
 
-// // ROUTER: /api/tags
-// const tagsRouter = require('./tags');
-// apiRouter.use('/tags', tagsRouter);
+// ROUTER: /api/petsitters
+const petsitterRouter = require('./petsitters');
+apiRouter.use('/petsitters', petsitterRouter);
 
 apiRouter.use((error, req, res, next) => {
   res.send(error);
