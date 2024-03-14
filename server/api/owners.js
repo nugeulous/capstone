23927a7c4339f7d0f5b48bdf134a164427e595f8
khirym <khmaynard@gmail.com>
2024-03-13 18:14:41 -0700

    token

--- server/api/owners.js
+++ server/api/owners.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
+
 const express = require('express');
 const ownersRouter = express.Router();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const { requireUser } = require('./utils');
+// const { JWT_SECRET } = process.env;
 
 const { 
   createOwner,
   getAllOwners,
   getOwnerById,
-  updateOwner
+  getOwnerByEmail,
+  getOwner
 } = require('../db/index');
 
 ownersRouter.get('/', async (req, res, next) => {
@@ -19,15 +26,47 @@ ownersRouter.get('/', async (req, res, next) => {
 
 ownersRouter.post('/register', async (req, res, next) => {
     try {
-      const { email, password, fname, lname, phone } = req.body;
+      const salt = await bcrypt.genSalt()
+      const hashedPassword = await bcrypt.hash(req.body.password, salt)
+      const { email, password, fname, lname, location, phone, image, gender } = req.body;
+      // Extract necessary properties from the request body
 
-      const owner = await createOwner({ email, password, fname, lname, phone});
-  
-      res.send(owner);
+      const owner = await createOwner({ email, password: hashedPassword, fname, lname, location, phone, image, gender });  
+      res.send({ owner });
     } catch ({ name, message }) {
       next({ name, message });
     }
   });
+
+  ownersRouter.post('/login', async (req, res, next) => {
+    const { email } = req.body;
+
+    // request must have both
+  if (!email) {
+    next({
+      name: 'MissingCredentialsError',
+      message: 'Please supply both a username and password'
+    });
+  }
+
+    try {
+        const owner = await getOwner(email);
+        if (!owner) {
+          next({
+            name: 'IncorrectCredentialsError',
+            message: 'Username or password is incorrect',
+          })
+        } else {
+          console.log(owner)
+          const token = jwt.sign({id: owner.id, email}, process.env.JWT_SECRET, { expiresIn: '1w', });
+          console.log(token)
+          res.send({ message: "you're logged in!", token,  });
+        } 
+      
+    } catch (error) {
+        next(error);
+    }
+});
 
   ownersRouter.get('/:id', async (req, res, next) => {
     try {
