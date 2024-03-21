d5dc1f311e5d6667f465d981cff6615f8174dc41
khirym <khmaynard@gmail.com>
2024-03-20 18:38:37 -0700

    about page and services links

---
+++ client/src/Components/DefaultLayout/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>OUR MISSION</h1>
+        <p>
+          Our mission is to provide a seamless experience for any and all dog
+          owners/lovers! We aim to provide all types of services and needs for
+          everyone's favorite furry friends, as well as allow our community to
+          engage with one another in ways they never had before in just one
+          central location.
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
