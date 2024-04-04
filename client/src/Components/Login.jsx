9258f1c11adf9416f585f4571137ecac10425233
monica bobadilla <bobadillamonica5@gmail.com>
2024-04-03 20:55:00 -0400

    fixed miltary time filter

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -32,7 +32,8 @@ function Copyright(props) {
   );
 }
 const defaultTheme = createTheme();
-const Login = ({ setToken, setRole, setUserID, setUser }) => {
+const Login = ({ setToken, setRole, setUser }) => {
+// const Login = ({ setToken, setRole, setUserID, setUser }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -46,7 +47,7 @@ const Login = ({ setToken, setRole, setUserID, setUser }) => {
       console.log(result.owner, "this is login result.owner");
       setRole(result.role);
       setToken(result.token);
-      setUserID(result.id);
+      // setUserID(result.id);
       setUser(result.owner);
     } catch (error) {
       setError("incorrect email or password");
