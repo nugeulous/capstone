2dd3f3adc01b2dc51083b3e7f13ce6a12db58261
Roy Watson-Badell <rwatsonbadell@roys-mbp.mynetworksettings.com>
2024-04-10 21:24:02 -0400

    some preliminary css and UI changes

---
+++ client/src/API/photoUploadApi.js
@@ -0,0 +1,15 @@
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+
+export const uploadPhoto = async (formData) => {
+  try {
+      const response = await fetch(`${API_URL}/photos/upload-by-file`, {
+          method: "POST",
+          body: formData, // Pass the FormData object directly as the request body
+      });
+      const result = await response.json();
+      return result;
+  } catch (error) {
+      console.error("Error uploading photo:", error);
+      throw error;
+  }
+};
