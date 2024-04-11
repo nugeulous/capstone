5928831e602a401ea7fe1c5313352dadb4caa50c
nugeulous <c.ryan.nugent@gmail.com>
2024-04-10 20:45:51 -0400

    who knows

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ console.log(__dirname);
 console.log(`${__dirname}/public/uploads`);
 app.use(cors());
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "..", "client/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use('/events', express.static(path.join(__dirname, 'public/uploads')));
 
