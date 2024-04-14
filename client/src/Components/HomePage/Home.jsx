3d4f74b2d2cf3741e01587944d0a4b9673376b73
khirym <khmaynard@gmail.com>
2024-04-14 16:35:00 -0700

    final upload

--- client/src/Components/HomePage/Home.jsx
+++ client/src/Components/HomePage/Home.jsx
@@ -2,61 +2,21 @@ import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import SlideShow from "./SlideShow";
-
+import Services from "../../Components/DefaultLayout/Services";
 export default function Home() {
   const navigate = useNavigate();
 
   return (
     <>
-
       <h1>Home</h1>
       <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-
-        }}>
-      <SlideShow />
-        </div>
-      <Services/>
-      
-
-      
-      {/* <Button
-        variant="outlined"
-        onClick={() => {
-          navigate(`/BookService`);
-        }}
-      >
-      Walkers
-      </Button>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          navigate(`/Groomers`);
-        }}
-      >
-      Groomers
-      </Button>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          navigate(`/Trainers`);
-        }}
-      >
-      Training
-      </Button>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          navigate(`/Sitters`);
-        }}
-      >
-        Pet Sitters
-      </Button> */}
-
-{/* Monica says order history is showing up on hers. Lets get whatever she has in here instead of this lonely button.  */}
-
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <SlideShow />
+      </div>
+      <Services />
       <Button
         variant="outlined"
         onClick={() => {
@@ -68,7 +28,7 @@ export default function Home() {
       <div>
         See all liked events
         <button
-        style={{cursor: "pointer"}}
+          style={{ cursor: "pointer" }}
           onClick={() => {
             navigate(`/liked-events`);
           }}
@@ -76,6 +36,6 @@ export default function Home() {
           here
         </button>
       </div>
-    </div>
+    </>
   );
 }
