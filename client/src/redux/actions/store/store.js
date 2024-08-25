38bff7bb98168bcdff0dff4bc09ef6b8cc00c7bc
monica bobadilla <bobadillamonica5@gmail.com>
2024-08-25 15:39:26 -0400

    added redux section to src

--- client/src/redux/actions/store/store.js
+++ client/src/redux/actions/store/store.js
@@ -1,6 +1,12 @@
 // configure store to manage state and reducers processing actions
-import { configureStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import bookingReducer from '../reducers/bookingReducer';
-const store = configureStore(bookingReducer);
+
+const store = configureStore({
+  reducer: {
+    booking: bookingReducer, // you can name the key based on the feature, e.g., 'booking'
+  },
+});
 
 export default store;
+
